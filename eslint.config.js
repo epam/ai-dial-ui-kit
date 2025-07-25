56d28476c01dc0c5e622a056b125a41a517bf95a
Polina <palina_kryvasheyeva@epam.com>
2025-07-25 15:06:07 +0200

    Add eslint plugins

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,64 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import storybook from 'eslint-plugin-storybook';
-
-import js from '@eslint/js';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
-import { globalIgnores } from 'eslint/config';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
-export default tseslint.config(
-  [
-    globalIgnores(['dist']),
-    {
-      files: ['**/*.{ts,tsx}'],
-      extends: [
-        js.configs.recommended,
-        tseslint.configs.recommended,
-        reactHooks.configs['recommended-latest'],
-        reactRefresh.configs.vite,
-      ],
-      languageOptions: {
-        ecmaVersion: 5,
-        globals: globals.browser,
-      },
-    },
-  ],
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default defineConfig([
+  globalIgnores([
+    '**/node_modules',
+    '**/dist',
+    '**/**.config.js',
+    '**/**.config.mjs',
+    '**/**.spec.ts',
+  ]),
   storybook.configs['flat/recommended'],
-);
+  {
+    extends: compat.extends(
+      'eslint:recommended',
+      'plugin:prettier/recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/stylistic',
+      'plugin:react-refresh/recommended',
+      'plugin:@typescript-eslint/recommended',
+    ),
+    plugins: ['@typescript-eslint'],
+    languageOptions: {
+      ecmaVersion: 5,
+      globals: globals.browser,
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^__',
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'prettier/prettier': 'error',
+      'no-empty': 'error',
+      'no-console': [
+        'error',
+        {
+          allow: ['warn', 'error', 'info'],
+        },
+      ],
+    },
+  },
+]);
