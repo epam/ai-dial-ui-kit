78ead3cc1a285aae34e700820b15e9c19970a07a
Nataliia Tolokontseva <129855502+hhharmEpm@users.noreply.github.com>
2025-09-30 21:24:56 +0300

    chore: Revert try to fix publish running twice (#33) (#35) [skip ci]

--- tools/publish-lib.mjs
+++ tools/publish-lib.mjs
@@ -67,11 +67,15 @@ try {
   console.error(`Error reading package.json file from library build output.`);
 }
 
-console.log(
-  `Running publish command: npm publish --access public --tag ${tag}`,
-);
-// using npx to avoid triggering npm scripts and therefore infinite publish loop
-execSync(`npx --yes npm@latest publish --access public --tag ${tag}`);
+try {
+  console.log(
+    `Running publish command: npm publish --access public --tag ${tag}`,
+  );
+  // Execute "npm publish" to publish
+  execSync(`npm publish --access public --tag ${tag}`);
+} catch {
+  console.error(`Publish failed.`);
+}
 
 function getDevVersion(potentialVersion) {
   let result;
