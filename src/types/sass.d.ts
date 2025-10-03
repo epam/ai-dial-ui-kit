065ffab5884aa96b2ab8ee0a2d05dd7a78327868
Valery Dluski <valerydluski@gmail.com>
2025-10-03 10:42:36 +0200

    chore: remove unused CSS module declaration from sass.d.ts

--- src/types/sass.d.ts
+++ src/types/sass.d.ts
@@ -7,8 +7,3 @@ declare module '*.scss' {
   const content: Record<string, string>;
   export default content;
 }
-
-declare module "*.css" {
-  const content: Record<string, string>;
-  export default content;
-}
