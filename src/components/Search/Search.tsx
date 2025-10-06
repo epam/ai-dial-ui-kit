7154b7241bc4c5e20028e319aa8d7a6464257b17
Mikhail Hahalushka <Mikhail_Hahalushka@epam.com>
2025-10-06 15:15:58 +0400

    merge search and search input components

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -3,11 +3,19 @@ import classNames from 'classnames';
 import { IconSearch, IconX } from '@tabler/icons-react';
 
 import { DialIcon } from '@/components/Icon/Icon';
-import { DialSearchInput, type DialSearchInputProps } from './SearchInput';
 import { SIZE_CONFIG } from './constants';
 import { SearchSize } from '@/types/search';
 
-export interface DialSearchProps extends DialSearchInputProps {
+export interface DialSearchProps {
+  elementId: string;
+  value?: string | number | null;
+  placeholder?: string;
+  disabled?: boolean;
+  readonly?: boolean;
+  invalid?: boolean;
+  cssClass?: string;
+  containerCssClass?: string;
+  onChange?: (value: string) => void;
   size?: SearchSize;
 }
 
@@ -29,68 +37,101 @@ export interface DialSearchProps extends DialSearchInputProps {
  *
  * @param elementId - Unique identifier for the input element
  * @param [value] - The current value of the input
- * @param [placeholder] - The placeholder text for the input
- * @param [size='base'] - The size of the search input. Supported values: `'small'` | `'base'`
- * @param [onChange] - Callback function called when the input value changes
+ * @param [placeholder] - Placeholder text shown when input is empty
  * @param [disabled=false] - Whether the input is disabled
+ * @param [readonly=false] - Whether the input is read-only (non-editable)
+ * @param [invalid=false] - Whether the input should be styled as invalid
+ * @param [cssClass] - Additional CSS classes applied to the input element
+ * @param [containerCssClass] - Additional CSS classes applied to the container
+ * @param [onChange] - Callback fired when the input value changes
+ * @param [size='base'] - The size of the search input. Supported values: `'small'` | `'base'`
  */
 export const DialSearch: FC<DialSearchProps> = ({
+  elementId,
+  value,
+  placeholder,
+  disabled,
+  readonly,
+  invalid,
+  cssClass,
+  containerCssClass,
   onChange,
   size = SearchSize.Base,
-  ...props
 }) => {
-  const [query, setQuery] = useState(props.value || '');
+  const [query, setQuery] = useState(value || '');
 
   useEffect(() => {
-    setQuery(props.value || '');
-  }, [props.value]);
+    setQuery(value || '');
+  }, [value]);
 
   const handleChange = useCallback(
-    (value: string) => {
-      setQuery(value);
-      onChange?.(value);
+    (newValue: string) => {
+      setQuery(newValue);
+      onChange?.(newValue);
     },
     [onChange],
   );
 
   const sizeConfig = SIZE_CONFIG[size];
 
+  const handleClear = useCallback(() => {
+    handleChange('');
+  }, [handleChange]);
+
   return (
-    <DialSearchInput
-      {...props}
-      value={query}
-      onChange={handleChange}
-      cssClass={classNames('w-full', props.cssClass, sizeConfig.textClass)}
-      containerCssClass={classNames(sizeConfig.containerClass)}
-      iconBeforeInput={
+    <div
+      className={classNames(
+        'dial-input flex flex-row items-center justify-between',
+        invalid && 'dial-input-error',
+        disabled && 'dial-input-disable',
+        readonly && 'dial-input-readonly',
+        sizeConfig.containerClass,
+        containerCssClass,
+      )}
+    >
+      <DialIcon
+        className={classNames(disabled ? 'text-secondary' : 'text-primary')}
+        icon={
+          <IconSearch
+            size={sizeConfig.iconSize}
+            stroke={sizeConfig.iconStroke}
+          />
+        }
+      />
+
+      <input
+        id={elementId}
+        type="text"
+        autoComplete="off"
+        placeholder={placeholder}
+        value={query ?? ''}
+        title={query ? String(query) : ''}
+        disabled={disabled}
+        readOnly={readonly}
+        className={classNames(
+          'border-0 bg-transparent w-full',
+          cssClass,
+          sizeConfig.textClass,
+        )}
+        onChange={(event) =>
+          !readonly && handleChange(event.currentTarget.value)
+        }
+      />
+
+      {query && !readonly && !disabled && (
         <DialIcon
-          className={classNames(
-            props.disabled ? 'text-secondary' : 'text-primary',
-          )}
+          className="text-primary cursor-pointer"
           icon={
-            <IconSearch
+            <IconX
               size={sizeConfig.iconSize}
               stroke={sizeConfig.iconStroke}
+              onClick={handleClear}
+              aria-label="Clear search"
+              role="button"
             />
           }
         />
-      }
-      iconAfterInput={
-        query ? (
-          <DialIcon
-            className="text-primary cursor-pointer"
-            icon={
-              <IconX
-                size={sizeConfig.iconSize}
-                stroke={sizeConfig.iconStroke}
-                onClick={() => handleChange('')}
-                aria-label="Clear search"
-                role="button"
-              />
-            }
-          />
-        ) : undefined
-      }
-    />
+      )}
+    </div>
   );
 };
