4420e45a3bacdd05c019c803cb0ec6549b8579e9
Palina_Kryvasheyeva <Palina_Kryvasheyeva@epam.com>
2025-10-13 11:34:41 +0200

    fix: input width (#74)

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -146,7 +146,7 @@ export const DialInput: FC<DialInputProps> = ({
           placeholder={placeholder}
           value={value ?? ''}
           disabled={disabled}
-          className={classNames('border-0 bg-transparent', cssClass)}
+          className={classNames('border-0 bg-transparent w-full', cssClass)}
           onChange={(event) => !readonly && handleChange?.(event)}
           onKeyDown={onKeyDown}
           onWheel={handleWheel}
