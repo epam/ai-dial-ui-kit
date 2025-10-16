6c574e35eb69dcd74e923611d99ec035c244c0f6
Mikhail Hahalushka <63146655+likeamike@users.noreply.github.com>
2025-10-16 17:22:30 +0400

    feat: allow scroll and dropdown when too many tabs (Issue #37) (#96)

--- src/components/Tabs/Tabs.spec.tsx
+++ src/components/Tabs/Tabs.spec.tsx
@@ -1,7 +1,8 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import { beforeEach, describe, expect, test, vi } from 'vitest';
+import { afterAll, beforeAll, describe, expect, test, vi } from 'vitest';
 import { DialTabs } from './Tabs';
 import { TabOrientation } from '@/types/tab';
+import * as useIsTabletScreenHook from '@/hooks/use-is-tablet-screen';
 
 const tabsMock = [
   { id: 'tab1', name: 'Tab1' },
@@ -9,14 +10,34 @@ const tabsMock = [
 ];
 
 describe('Dial UI Kit :: DialTabs', () => {
-  beforeEach(() => {
-    vi.resetAllMocks();
+  let originalResizeObserver: typeof ResizeObserver;
+
+  beforeAll(() => {
+    originalResizeObserver = globalThis.ResizeObserver;
+
+    class ResizeObserverMock implements ResizeObserver {
+      callback: ResizeObserverCallback;
+      constructor(callback: ResizeObserverCallback) {
+        this.callback = callback;
+      }
+      observe = vi.fn();
+      unobserve = vi.fn();
+      disconnect = vi.fn();
+    }
+
+    globalThis.ResizeObserver =
+      ResizeObserverMock as unknown as typeof ResizeObserver;
+  });
+
+  afterAll(() => {
+    vi.restoreAllMocks();
+    globalThis.ResizeObserver = originalResizeObserver;
   });
 
   test('renders horizontal tabs and handles click', () => {
     const onClick = vi.fn();
     render(<DialTabs tabs={tabsMock} activeTab="tab1" onClick={onClick} />);
-    expect(screen.getAllByText('Tab1').length).toBe(2);
+    expect(screen.getAllByText('Tab1').length).toBe(1);
     expect(screen.getAllByText('Tab2').length).toBe(1);
     fireEvent.click(screen.getByText('Tab2'));
     expect(onClick).toHaveBeenCalledWith('tab2');
@@ -31,7 +52,54 @@ describe('Dial UI Kit :: DialTabs', () => {
         orientation={TabOrientation.Vertical}
       />,
     );
-    expect(screen.getAllByText('Tab1').length).toBe(2);
+    expect(screen.getAllByText('Tab1').length).toBe(1);
     expect(screen.getAllByText('Tab2').length).toBe(1);
+  });
+
+  test('shows dropdown button when when mobile view', () => {
+    const onClick = vi.fn();
+
+    vi.spyOn(useIsTabletScreenHook, 'useIsTabletScreen').mockReturnValue(true);
+
+    const { container } = render(
+      <DialTabs tabs={tabsMock} activeTab="tab1" onClick={onClick} />,
+    );
+
+    const chevronIcon = container.querySelector('.tabler-icon-chevron-down');
+    expect(chevronIcon).toBeInTheDocument();
+  });
+
+  test('handles tab click in mobile view', () => {
+    const onClick = vi.fn();
+
+    vi.spyOn(useIsTabletScreenHook, 'useIsTabletScreen').mockReturnValue(true);
+
+    render(<DialTabs tabs={tabsMock} activeTab="tab1" onClick={onClick} />);
+
+    const tab = screen.getByRole('tab');
+    expect(tab).toBeInTheDocument();
+
+    fireEvent.click(tab);
+
+    expect(onClick).toHaveBeenCalledTimes(1);
+    expect(onClick).toHaveBeenCalledWith('tab1');
+  });
+
+  test('handles tab click in desktop view', () => {
+    const onClick = vi.fn();
+
+    vi.spyOn(useIsTabletScreenHook, 'useIsTabletScreen').mockReturnValue(false);
+
+    render(<DialTabs tabs={tabsMock} activeTab="tab1" onClick={onClick} />);
+
+    const tab2 = screen
+      .getAllByRole('tab')
+      .find((el) => el.textContent === 'Tab2');
+    expect(tab2).toBeInTheDocument();
+
+    fireEvent.click(tab2!);
+
+    expect(onClick).toHaveBeenCalledTimes(1);
+    expect(onClick).toHaveBeenCalledWith('tab2');
   });
 });
