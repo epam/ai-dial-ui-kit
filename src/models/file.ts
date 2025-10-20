fc152be6e44038426d3f2623553da2ab54a74a31
Mikhail Hahalushka <Mikhail_Hahalushka@epam.com>
2025-10-20 12:36:52 +0400

    base file model

---
+++ src/models/file.ts
@@ -0,0 +1,42 @@
+import type { DialModifiedEntity } from './base-entity';
+
+export interface DialFile extends DialModifiedEntity {
+  bucket?: string;
+  contentLength?: number;
+  contentType?: string;
+  nodeType: DialFileNodeType;
+  parentPath?: string | null;
+  resourceType?: DialFileResourceType;
+  url?: string;
+  children?: DialFile[];
+  items?: DialFile[];
+  path: string;
+  name?: string;
+  folderId: string;
+  updateTime: string;
+  author?: string;
+  nextToken?: string;
+  extension?: string;
+  id?: string;
+  etag?: string;
+  permissions?: DialFilePermission[];
+}
+
+export enum DialFileNodeType {
+  ITEM = 'item',
+  FOLDER = 'folder',
+}
+
+export enum DialFileResourceType {
+  FILE = 'FILE',
+  PROMPT = 'PROMPT',
+  CONVERSATION = 'CONVERSATION',
+  APPLICATION = 'APPLICATION',
+  TOOLSET = 'TOOL_SET',
+}
+
+export enum DialFilePermission {
+  READ = 'READ',
+  WRITE = 'WRITE',
+  SHARE = 'SHARE',
+}
