116321ea4a8cac0434abc027706c8b5c5ee83f27
Mikhail Hahalushka <63146655+likeamike@users.noreply.github.com>
2025-10-23 12:37:00 +0400

    feat: add file model (Issue #114) (#113)

---
+++ src/models/file.ts
@@ -0,0 +1,39 @@
+import type { DialModifiedEntity } from './base-entity';
+
+export interface DialFile extends DialModifiedEntity {
+  bucket?: string;
+  contentLength?: number;
+  contentType?: string;
+  nodeType: DialFileNodeType;
+  parentPath?: string | null;
+  resourceType?: DialFileResourceType;
+  url?: string;
+  items?: DialFile[];
+  path: string;
+  name?: string;
+  folderId: string;
+  author?: string;
+  nextToken?: string;
+  extension?: string;
+  id?: string;
+  permissions?: DialFilePermission[];
+}
+
+export enum DialFileNodeType {
+  ITEM = 'item',
+  FOLDER = 'folder',
+}
+
+export enum DialFileResourceType {
+  FILE = 'FILE',
+  PROMPT = 'PROMPT',
+  CONVERSATION = 'CONVERSATION',
+  APPLICATION = 'APPLICATION',
+  TOOLSET = 'TOOL_SET',
+}
+
+export enum DialFilePermission {
+  READ = 'READ',
+  WRITE = 'WRITE',
+  SHARE = 'SHARE',
+}
