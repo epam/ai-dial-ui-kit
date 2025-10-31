70ebaee24c308f4a0b0bfb2cdb42ca8746e37e2f
Polina <palina_kryvasheyeva@epam.com>
2025-10-31 17:16:56 +0100

    fix swtyle

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -420,7 +420,7 @@ export const DialSelect: FC<DialSelectProps> = ({
           size === SelectSize.Sm
             ? 'min-h-[25px] px-1.5 py-1'
             : 'min-h-[38px] px-3 py-2',
-          variant === SelectVariant.Secondary ? 'bg-layer-4' : '',
+          variant === SelectVariant.Secondary ? '!bg-layer-4' : '',
           cssClass,
         )}
         onClick={() => !disabled && setOpen((v) => !v)}
