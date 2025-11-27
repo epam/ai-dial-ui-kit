60d50789d1aebddcc014aaa02ba1c256124ba11a
Polina <palina_kryvasheyeva@epam.com>
2025-11-27 19:13:57 +0100

    chore: rename Alert and DraggableItem props to standard naming

--- src/components/DraggableItem/constants.tsx
+++ src/components/DraggableItem/constants.tsx
@@ -1,5 +1,5 @@
 export const DRAG_TYPE = 'column';
 
-export const containerBaseClasses = 'flex items-center';
+export const containerBaseClassName = 'flex items-center';
 
-export const handleBaseClasses = 'mr-3 cursor-move text-secondary';
+export const handleBaseClassName = 'mr-3 cursor-move text-secondary';
