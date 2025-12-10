1df100fa77051d9ccf2b9545a7f1992bac7b3272
Valery Dluski <valerydluski@gmail.com>
2025-12-10 16:05:10 +0100

    fix: correct height value in panelBaseClassName constant

--- src/components/FileManager/components/FileManagerNavigationPanel/constants.tsx
+++ src/components/FileManager/components/FileManagerNavigationPanel/constants.tsx
@@ -1,5 +1,5 @@
 export const panelBaseClassName =
-  'w-full h-[12] flex items-center justify-between gap-4';
+  'w-full flex items-center justify-between gap-4';
 
 export const breadcrumbContainerClassName =
   'min-w-0 h-[38px] flex-1 overflow-hidden bg-layer-3 px-3 flex items-center rounded';
