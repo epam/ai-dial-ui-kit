62e2fc4269462e4ca1d1cced2746956b60d67ef3
Valery Dluski <valerydluski@gmail.com>
2025-12-17 21:06:06 +0100

    feat: enhance DialErrorText component with className and aria-label props for better styling and accessibility

--- src/components/ErrorText/ErrorText.tsx
+++ src/components/ErrorText/ErrorText.tsx
@@ -1,21 +1,22 @@
-import type { FC } from 'react';
+import { mergeClasses } from '@/utils/merge-classes';
+import type { FC, HTMLAttributes } from 'react';
 
-export interface Props {
+export interface Props extends HTMLAttributes<HTMLSpanElement> {
   errorText?: string;
 }
 
 /**
  * A component for displaying error messages with consistent styling
- *
- * @example
- * ```tsx
- * <DialErrorText errorText="This field is required" />
- * ```
- *
- * @param [errorText] - The error message text to display. If undefined or empty, nothing is rendered
  */
-export const DialErrorText: FC<Props> = ({ errorText }) => {
+export const DialErrorText: FC<Props> = ({ errorText, className, ...rest }) => {
+  if (!errorText) return null;
+
   return (
-    errorText && <span className="text-error dial-tiny mt-1">{errorText}</span>
+    <span
+      {...rest}
+      className={mergeClasses('text-error dial-tiny mt-1', className)}
+    >
+      {errorText}
+    </span>
   );
 };
