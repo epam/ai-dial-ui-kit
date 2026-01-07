18e87eeba2335ec35e3afeda3cdb62ae526fcf52
Polina <palina_kryvasheyeva@epam.com>
2026-01-07 21:02:40 +0100

    fix: button paddings when only icon

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -76,14 +76,11 @@ export const DialButton: FC<DialButtonProps> = ({
     textClassName,
   );
 
+  const isOnlyIcon = !variant && !label && (iconAfter || iconBefore);
   const btnClassName = classNames(
     variant && getButtonClassNames(variant, appearance),
     size === ButtonSize.Small ? 'dial-button-small' : 'dial-button-standard',
-    !label && (iconAfter || iconBefore)
-      ? size === ButtonSize.Small
-        ? 'px-2'
-        : 'px-3'
-      : '',
+    !isOnlyIcon && (size === ButtonSize.Small ? 'px-2' : 'px-3'),
     'disabled:cursor-not-allowed focus-visible:outline outline-offset-0',
     className,
   );
