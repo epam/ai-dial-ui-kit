73b3fda4acef77e55628b072e2ffa6e85a805acb
Palina_Kryvasheyeva <Palina_Kryvasheyeva@epam.com>
2026-01-30 14:10:08 +0100

    fix: Icon Button add tooltip props (#470)

--- src/components/IconButton/IconButton.stories.tsx
+++ src/components/IconButton/IconButton.stories.tsx
@@ -77,7 +77,7 @@ export const NeutralOutlinedIconButton: Story = {
 
 export const IconButtonWithTooltip: Story = {
   args: {
-    tooltipText: 'Custom tooltip text',
+    tooltipProps: { tooltip: 'Custom tooltip text' },
     variant: ButtonVariant.Neutral,
     appearance: ButtonAppearance.Outlined,
   },
