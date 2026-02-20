755479574183cc004c02f02090468f1afdd5d923
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-02-20 23:25:21 +0100

    chore: move storybook [skip ci]

--- src/components/RemoveButton/RemoveButton.stories.tsx
+++ src/components/RemoveButton/RemoveButton.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react-vite';
 import { DialRemoveButton, type DialRemoveButtonProps } from './RemoveButton';
 
 const meta: Meta<typeof DialRemoveButton> = {
-  title: 'Utility/RemoveButton',
+  title: 'DIAL/Elements/Buttons/RemoveButton',
   component: DialRemoveButton,
   parameters: {
     layout: 'padded',
