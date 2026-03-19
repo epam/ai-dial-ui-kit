a37e35ad174d8310d46630cd013eac46bd370604
Palina_Kryvasheyeva <Palina_Kryvasheyeva@epam.com>
2026-03-19 16:07:37 +0100

    fix: reset conflict state after close popup and filters for grid (#573)

--- src/components/FileManager/FileManager.tsx
+++ src/components/FileManager/FileManager.tsx
@@ -1413,6 +1413,14 @@ export const DialFileManagerView: FC = () => {
                   headerHeight: COMPACT_VIEW_HEADER_HEIGHT,
                   rowHeight: COMPACT_VIEW_HEADER_HEIGHT,
                   rowClass: 'group/grid-row',
+                  defaultColDef: {
+                    ...forwardedGridOptions.additionalGridOptions
+                      ?.defaultColDef,
+                    floatingFilter: navigationPanelOptions?.searchable
+                      ? false
+                      : forwardedGridOptions.additionalGridOptions
+                          ?.defaultColDef?.floatingFilter,
+                  },
                   ...(isCompactView
                     ? {
                         getRowHeight: (params) =>
