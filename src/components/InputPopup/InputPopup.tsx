d028e69093b3c82b440e212cf00cbbbf934c3ca1
Yauheni Osipau <yauheni_osipau@epam.com>
2026-04-08 17:39:04 +0300

    fix: added prop

--- src/components/InputPopup/InputPopup.tsx
+++ src/components/InputPopup/InputPopup.tsx
@@ -82,6 +82,7 @@ export const DialInputPopup: FC<DialInputPopupProps> = ({
   placeholder,
   editable = false,
   onValueChange,
+  collapseTagOverflow = false,
 }) => {
   const hasMultipleValues =
     Array.isArray(selectedValue) && selectedValue.length > 0;
@@ -203,7 +204,7 @@ export const DialInputPopup: FC<DialInputPopupProps> = ({
         <DialAutocompleteInputValue
           placeholder={placeholder}
           selectedItems={value as string[]}
-          collapseTagOverflow
+          collapseTagOverflow={collapseTagOverflow}
         />
         {!disabled && (
           <div className="ml-1">
