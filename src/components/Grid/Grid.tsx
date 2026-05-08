f64db3b041fa6a0c1a8defd203183fdb0f22cac9
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-05-09 00:39:41 +0200

    fix grid checkbox

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -49,6 +49,7 @@ import {
 
 import { debounceFn } from '@/utils/debounce.ts';
 import { ariaDescription } from '@/components/Checkbox/constants';
+import { useIsMobileScreen } from '@/hooks/use-is-mobile-screen';
 
 setupAgTestIds({ testIdAttribute: 'dataQA' });
 
@@ -197,6 +198,7 @@ export const DialGrid = <T extends object>({
   const [rowHeight, setRowHeight] = useState<number>(ROW_HEIGHT);
   const [gridApi, setGridApi] = useState<GridApi<T> | undefined>();
   const a11yId = useId();
+  const isMobileScreen = useIsMobileScreen();
   const selectedNodesRef = useRef<Set<string>>(new Set());
   const lastSelectionSourceRef = useRef<SelectionEventSourceType | undefined>(
     undefined,
@@ -326,18 +328,26 @@ export const DialGrid = <T extends object>({
     const baseClass = 'dial-row-select';
     const visibleClass = 'dial-row-select-visible';
 
+    if (isMobileScreen || (selectedRowIds && selectedRowIds.size > 0)) {
+      return `${baseClass} ${visibleClass}`;
+    }
+
     if (!selectedRowIds || selectedRowIds.size === 0) {
       return baseClass;
     }
     return `${baseClass} ${visibleClass}`;
-  }, [selectedRowIds]);
+  }, [isMobileScreen, selectedRowIds]);
 
   const getHeaderSelectionClasses = useCallback(() => {
+    if (isMobileScreen) {
+      return getSelectionClasses();
+    }
+
     if (!selectedRowIds || selectedRowIds.size === 0) {
       return 'dial-row-not-select-header';
     }
     return getSelectionClasses();
-  }, [getSelectionClasses, selectedRowIds]);
+  }, [getSelectionClasses, isMobileScreen, selectedRowIds]);
 
   const selectionColumnDef = useMemo(() => {
     if (selectionMode === GridSelectionMode.SINGLE) {
