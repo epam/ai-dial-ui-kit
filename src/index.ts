3c0d109b773d2059a43b039db9a4771be550a1f0
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-05-12 18:12:53 +0200

    feat: add Rounded Button

--- src/index.ts
+++ src/index.ts
@@ -46,7 +46,9 @@ export {
   DialPrimaryButton,
   DialGhostButton,
   DialLinkButton,
+  DialRoundedButton,
 } from './components/Button/ButtonWrappers';
+export type { DialRoundedButtonProps } from './components/Button/ButtonWrappers';
 export {
   DialErrorIconButton,
   DialGhostIconButton,
