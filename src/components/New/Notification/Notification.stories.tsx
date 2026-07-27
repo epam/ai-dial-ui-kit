cf3b1210ff5968408403004b8c3280a5a70424c8
Valery Dluski <valerydluski@gmail.com>
2026-07-27 16:23:20 +0200

    feat: update titles in CardShell and Notification stories to include versioning

--- src/components/New/Notification/Notification.stories.tsx
+++ src/components/New/Notification/Notification.stories.tsx
@@ -15,7 +15,7 @@ import {
 } from './NotificationWrapper';
 
 const meta = {
-  title: 'Components/Notification',
+  title: 'Components_2.0/Notification',
   component: Notification,
   parameters: {
     layout: 'padded',
